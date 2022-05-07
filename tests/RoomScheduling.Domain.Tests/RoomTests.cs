06fdb9dacba131434a5d77c1945e8ce084c6ca08
Marcin Sikorski <sikorski.m.p@gmail.com>
2022-05-07 10:18:09 +0200

    Define room test and set central nuget management
    Set up test project.
    Introduce central nuget management.
    Define room failing unit test.

---
+++ tests/RoomScheduling.Domain.Tests/RoomTests.cs
@@ -0,0 +1,25 @@
+using Xunit;
+using FluentAssertions;
+
+namespace RoomScheduling.Domain.Tests;
+
+public class RoomTests
+{
+    [Fact]
+    public void Define_room_creates_fully_configured_room()
+    {
+        //Arrange
+        var numberOfSeats = 10;
+        var hasProjector = true;
+        var hasSoundSystem = true;
+        var hasAirConditioner = false;
+        //Act
+        dynamic definedRoom = null;
+
+        //Assert
+        definedRoom.NumberOfSeats.Should().Be(numberOfSeats);
+        definedRoom.HasProjector.Should().Be(hasProjector);
+        definedRoom.HasSoundSystem.Should().Be(hasSoundSystem);
+        definedRoom.HasAirConditioner.Should().Be(hasAirConditioner);
+    }
+}
