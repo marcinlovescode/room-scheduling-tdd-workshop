8b9972d96ae3331f5e98e3713c5c20e52cf7d656
Marcin Sikorski <sikorski.m.p@gmail.com>
2022-05-09 15:49:42 +0200

    Read Room query handler test

--- tests/RoomScheduling.Application.Tests/RoomHandlersTests.cs
+++ tests/RoomScheduling.Application.Tests/RoomHandlersTests.cs
@@ -2,13 +2,14 @@ using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using RoomScheduling.Application.Handlers;
+using RoomScheduling.Domain;
 using RoomScheduling.Fixtures;
 using RoomScheduling.SqlitePersistence;
 using Xunit;
 
 namespace RoomScheduling.Application.Tests;
 
-public class DefineRoomTests
+public class RoomHandlersTests
 {
     [Fact]
     public async Task Defines_and_persists_room()
@@ -24,5 +25,21 @@ public class DefineRoomTests
         //Assert
         var persistedRoom = await dao.Get(command.Name);
         persistedRoom.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public async Task Returns_room()
+    {
+        //Arrange
+        var query = Guid.NewGuid().ToString("N");
+        var createDbConnection = DbFixture.GetDefaultCreateDbFunc();
+        await new Bootstrapper(createDbConnection).Bootstrap();
+        var dao = new RoomDao(createDbConnection);
+        await dao.Save(new Room(10, false, false, false, query));
+        var queryHandler = new ReadRoomHandler(dao);
+        //Act
+        var result = await queryHandler.Handle(query);
+        //Assert
+        result.Should().NotBeNull();
     }
 }
