f69542a7e709196ef66ac3c38b93b3d4f7028042
Marcin Sikorski <sikorski.m.p@gmail.com>
2022-05-18 18:47:55 +0200

    Save and Read DailySchedule integration test

---
+++ tests/RoomScheduling.SqlitePersistence.Tests/DailyScheduleDaoTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using RoomScheduling.Domain;
+using RoomScheduling.Fixtures;
+using Xunit;
+
+namespace RoomScheduling.SqlitePersistence.Tests;
+
+public class DailyScheduleDaoTests
+{
+    [Fact]
+    public async Task Saves_resource_booking()
+    {
+        //Arrange
+        var createDbConnection = DbFixture.GetDefaultCreateDbFunc();
+        var dbBootstrapper = new Bootstrapper(createDbConnection);
+        await dbBootstrapper.Bootstrap();
+        var dailySchedule = new DailySchedule($"{Guid.NewGuid():N}", DateOnly.FromDateTime(DateTime.Now));
+        var from = new TimeOnly(10, 00);
+        var to = new TimeOnly(12, 00);
+        dailySchedule.Book(new TimeOnly(10, 00), new TimeOnly(12, 00));
+        var dao = new DailyScheduleDao(createDbConnection);
+        //Act
+        await dao.Save(dailySchedule);
+        //Assert
+        var dailyScheduleFromDb = await dao.Get(dailySchedule.ResourceId, dailySchedule.Date);
+        dailyScheduleFromDb.IsTimeSlotAvailable(from, to).Should().BeFalse();
+    }
+}
