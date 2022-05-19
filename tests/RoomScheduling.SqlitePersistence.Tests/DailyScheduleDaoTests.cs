a23e0cca85414329bc440d44d71e3d0e180a689c
Marcin Sikorski <sikorski.m.p@gmail.com>
2022-05-19 14:07:29 +0200

    Read schedule for multiple resources test

--- tests/RoomScheduling.SqlitePersistence.Tests/DailyScheduleDaoTests.cs
+++ tests/RoomScheduling.SqlitePersistence.Tests/DailyScheduleDaoTests.cs
@@ -45,4 +45,24 @@ public class DailyScheduleDaoTests
         result.ResourceId.Should().Be(notExistingResourceId);
         result.Date.Should().Be(anyDate);
     }
+    
+    [Fact]
+    public async Task Returns_schedules_for_multiple_resources()
+    {
+        //Arrange
+        var createDbConnection = DbFixture.GetDefaultCreateDbFunc();
+        var dbBootstrapper = new Bootstrapper(createDbConnection);
+        var notExistingResourceId1 = $"{Guid.NewGuid():N}";
+        var notExistingResourceId2 = $"{Guid.NewGuid():N}";
+        var anyDate = DateOnly.MinValue;
+        await dbBootstrapper.Bootstrap();
+        var dao = new DailyScheduleDao(createDbConnection);
+        //Act
+        var result = await dao.Get(new []{notExistingResourceId1, notExistingResourceId2}, DateOnly.MinValue);
+        //Assert
+        result.ElementAt(0).Bookings.Should().HaveCount(0);
+        result.ElementAt(1).Bookings.Should().HaveCount(0);
+        result.ElementAt(0).Date.Should().Be(anyDate);
+        result.ElementAt(1).Date.Should().Be(anyDate);
+    }
 }
