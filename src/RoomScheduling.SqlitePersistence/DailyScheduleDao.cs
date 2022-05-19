8c7e7cac997d12e69fadf8451c91693f2f7f3b42
Marcin Sikorski <sikorski.m.p@gmail.com>
2022-05-19 14:19:51 +0200

    Read schedule for multiple resources impl

--- src/RoomScheduling.SqlitePersistence/DailyScheduleDao.cs
+++ src/RoomScheduling.SqlitePersistence/DailyScheduleDao.cs
@@ -17,7 +17,7 @@ public class DailyScheduleDao
     private const string ReadSqlQuery =
         @"SELECT ResourceId, [Date], [From], [To]
           FROM Bookings
-          WHERE ResourceId=@ResourceId AND [Date]=@Date";
+          WHERE ResourceId in @ResourcesIds AND [Date]=@Date";
 
     public DailyScheduleDao(Func<SqliteConnection> createDbConnection)
     {
@@ -29,7 +29,7 @@ public class DailyScheduleDao
         await using var connection = _createDbConnection();
         await connection.ExecuteAsync(InsertSqlCommand, DailyScheduleBookingDbModel.FromDomain(dailySchedule));
     }
-    
+
     public async Task<DailySchedule> Get(string resourceId, DateOnly date)
     {
         await using var connection = _createDbConnection();
@@ -39,5 +39,20 @@ public class DailyScheduleDao
             Date = date.ToShortDateString()
         });
         return DailyScheduleBookingDbModel.ToDomain(roomDbModel.ToList(), resourceId, date);
+    }
+
+    public async Task<IReadOnlyCollection<DailySchedule>> Get(string[] resources, DateOnly date)
+    {
+        await using var connection = _createDbConnection();
+        var roomDbModel = (await connection.QueryAsync<DailyScheduleBookingDbModel>(ReadSqlQuery, new
+        {
+            ResourcesIds = resources,
+            Date = date.ToShortDateString()
+        })).ToList();
+        var notExistingBookings = resources.Except(roomDbModel.Select(x => x.ResourceId).Distinct())
+            .Select(x => DailyScheduleBookingDbModel.ToDomain(new DailyScheduleBookingDbModel[] { }, x, date));
+        var bookingsByRoom = roomDbModel.GroupBy(x => x.ResourceId).ToDictionary(x => x.Key, x => x.ToList())
+            .Select(x => DailyScheduleBookingDbModel.ToDomain(x.Value, x.Key, date));
+        return notExistingBookings.Concat(bookingsByRoom).ToList();
     }
 }
