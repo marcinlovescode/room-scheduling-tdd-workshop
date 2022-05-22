c055e5ef8e6b86e3ff29981550aa77e5950b56d4
Marcin Sikorski <sikorski.m.p@gmail.com>
2022-05-22 13:40:18 +0200

    Send email about bookings test
    Use SendGrid as a mail client.
    Mock SendGrid client using Moq library.

--- tests/RoomScheduling.Application.Tests/RoomHandlersTests.cs
+++ tests/RoomScheduling.Application.Tests/RoomHandlersTests.cs
@@ -1,10 +1,17 @@
 using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Moq;
 using RoomScheduling.Application.Handlers;
 using RoomScheduling.Domain;
 using RoomScheduling.Fixtures;
 using RoomScheduling.SqlitePersistence;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using Xunit;
 
 namespace RoomScheduling.Application.Tests;
@@ -26,7 +33,7 @@ public class RoomHandlersTests
         var persistedRoom = await dao.Get(command.Name);
         persistedRoom.Should().NotBeNull();
     }
-    
+
     [Fact]
     public async Task Returns_room()
     {
@@ -41,5 +48,35 @@ public class RoomHandlersTests
         var result = await queryHandler.Handle(query);
         //Assert
         result.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task Sends_email_with_bookings_for_room()
+    {
+        //Arrange
+        var command = new { name = Guid.NewGuid().ToString("N"), date = DateOnly.FromDateTime(DateTime.Now), email="test@email.com" };
+        var createDbConnection = DbFixture.GetDefaultCreateDbFunc();
+        await new Bootstrapper(createDbConnection).Bootstrap();
+        var roomDao = new RoomDao(createDbConnection);
+        var scheduleDao = new DailyScheduleDao(createDbConnection);
+        var room = new Room(5, true, true, true, command.name);
+        var schedule = new DailySchedule(room.Name, command.date,
+            new (TimeOnly from, TimeOnly to)[] { (new TimeOnly(10, 00), new TimeOnly(13, 00)), (new TimeOnly(14, 00), new TimeOnly(15, 00))});
+        await roomDao.Save(room);
+        await scheduleDao.Save(schedule);
+        var sendGridResponse = new Response(HttpStatusCode.Accepted, new StringContent("OK"), null);
+        var sendGridMock = new Mock<ISendGridClient>();
+        SendGridMessage sendGridMessageToAssert = null;
+        sendGridMock.Setup(x => x.SendEmailAsync(It.IsAny<SendGridMessage>(), It.IsAny<CancellationToken>()))
+            .Callback((SendGridMessage message) => { sendGridMessageToAssert = message; })
+            .Returns(Task.FromResult(sendGridResponse));
+        var commandHandler = new SendNotificationAboutBookings(scheduleDao, sendGridMock.Object);
+        //Act
+        var result = await commandHandler.Handle(command);
+        //Assert
+        if (sendGridMessageToAssert == null)
+            throw new Exception("SendGrid client not called");
+        sendGridMessageToAssert.Personalizations.SelectMany(x => x.Tos).Select(x => x.Email).Should().Contain(command.email);
+        sendGridMessageToAssert.HtmlContent.Should().Be("Bookings: 10:00-13:00, 14:00-15:00");
     }
 }
